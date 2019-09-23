3b546fb711259f0dd5f6284a47b6ee36621d9f16
koustavmandal95 <koustavmandal95@gmail.com>
2019-09-23 10:13:07 +0530

    Find the number of digits in number.

---
+++ No_of_digits.py
@@ -0,0 +1,14 @@
+def digits(N,digit):
+    if N==0:
+        return digit
+    else:
+        digit=digit+1
+    return digits(N//10,digit)
+
+if __name__=="__main__":
+    N=int(input())
+    print(digits(N,digit=0))
+'''
+156
+3
+'''
