3f84a8d62ee12517b5f0324bfa2d807f79cf89ba
koustavmandal95 <koustavmandal95@gmail.com>
2020-03-01 08:16:26 +0530

    Update pascal_trinagle.cpp

--- cdac_cpp_files/pascal_trinagle.cpp
+++ cdac_cpp_files/pascal_trinagle.cpp
@@ -12,17 +12,18 @@ int main()
     arr[i]=0;
     store[i]=0;
   }
-  arr[0] = 1;
+  store[0] =1;
+  arr[0]=1;
   for(int i=0;i<N;i++)
   {
     for(int j=1;j<i;j++)
     {
-      store[j-1]=arr[j-1];
-      store[j] = arr[j];
+      arr[j] = store[j-1]+store[j];
     }
     arr[i]=1;
     for(int k=0;k<N;k++)
     {
+      store[k]=arr[k];
       cout << arr[k] << " " ;
     }
     cout << "\n";
