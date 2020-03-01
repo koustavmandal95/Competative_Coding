7b82fee420b59366a2944f2fafcb971e3081113d
koustavmandal95 <koustavmandal95@gmail.com>
2020-03-01 16:57:43 +0530

    Update Dynamic_allocation_2D.cpp

--- cdac_cpp_files/Dynamic_allocation_2D.cpp
+++ cdac_cpp_files/Dynamic_allocation_2D.cpp
@@ -1,5 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+void print(int **matrix, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0; j<n;j++)
+        {
+            cout << matrix[i][j] << "  ";
+        }
+        cout << "\n" ;
+    }
+}
+void fill_all(int **matrix, int n)
+{
+      int row = n;
+      int col = n;
+    for ( int i=1; i<=n; i++)
+    {
+      if (matrix[0][col/2+i] ==0)
+      {
+          matrix[0][col/2+i]=i;
+      }
+      else if(matrix[n-1+i][col/2+1+i]==0)
+      {
+         matrix[n-1][col/2+1] = i; 
+      }
+    }
+    print(matrix,n);
+
+}
 void fill_all(int **matrix, int n)
 {
         for(int i =0;i<n;i++){
